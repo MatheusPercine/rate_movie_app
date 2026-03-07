65255e002d93c1164fd1d87e974f6c6078577234
MatheusPercine <matheuspercine@gmail.com>
2026-03-07 00:21:21 -0300

    remoção de arquivos

--- frontend/SMSAgenda/src/App.tsx
+++ frontend/SMSAgenda/src/App.tsx
@@ -1,19 +1,6 @@
 import { Routes, Route, Navigate, Outlet } from "react-router-dom";
-import Agenda from "./pages/Agenda.tsx";
-import NovoAgendamento from "./pages/NovoAgendamento.tsx";
 import { CompleteLayout } from "./components/layout.tsx";
-import GerenciarAgendamentos from "./pages/GerenciarAgendamentos.tsx";
-import MeusAgendamentos from "./pages/MeusAgendamentos.tsx"; // tela não-essencial e em desenvolvimento
-import ResetPasswordForm from "./pages/reset-password-form.tsx";
-import LoginForm from "./pages/login.tsx";
-import VerifyForm from "./pages/verify-form.tsx";
-import ForgotPasswordForm from "./pages/forgot-password-form.tsx";
-import DescricaoAgendamento from "./pages/DescricaoAgendamento.tsx";
-import GerenciarUsuarios from "./pages/GerenciarUsuarios.tsx";
-import Suporte from "./pages/Suporte.tsx";
 import PrivateRoute from "./routes/PrivateRoute";
-import RegisterForm from "./pages/register.tsx";
-import GerenciarSalas from "./pages/GerenciarSalas.tsx";
 
 import SearchPage from "./pages/SearchPage.tsx";
 import { useState } from "react";
@@ -95,97 +82,6 @@ function App() {
         <Route path="/rated" element={<RatedMoviesPage />} />
         <Route path="/not_found" element={<NotFound />} />
       </Route>
-      <Route
-        path="/gerenciar_salas"
-        element={
-          <PrivateRoute>
-            <CompleteLayout>
-             <GerenciarSalas/>
-            </CompleteLayout>
-          </PrivateRoute>
-        }
-      ></Route>
-      <Route
-        path="/gerenciar_usuarios"
-        element={
-          <PrivateRoute>
-            <CompleteLayout>
-             <GerenciarUsuarios/>
-            </CompleteLayout>
-          </PrivateRoute>
-        }
-      ></Route>
-      <Route
-        path="/gerenciar_agendamentos"
-        element={
-          <PrivateRoute>
-            <CompleteLayout>
-             <GerenciarAgendamentos/>
-            </CompleteLayout>
-          </PrivateRoute>
-        }
-      ></Route>
-      <Route
-        path="/descricao_agendamento/:id"
-        element={
-          <PrivateRoute>
-            <CompleteLayout>
-             <DescricaoAgendamento/>
-            </CompleteLayout>
-          </PrivateRoute>
-        }
-      ></Route>
-      <Route 
-        path="/suporte"
-        element={
-          <PrivateRoute>
-            <CompleteLayout>
-              <Suporte />
-            </CompleteLayout>
-          </PrivateRoute>
-        }
-      ></Route>
-      <Route
-        path="/agenda"
-        element={
-          <PrivateRoute>
-            <CompleteLayout>
-              <Agenda />
-            </CompleteLayout>
-          </PrivateRoute>
-        }
-        
-      ></Route> 
-      <Route 
-        path="/novo_agendamento"
-        element={
-          <PrivateRoute>
-            <CompleteLayout>
-              <NovoAgendamento />
-            </CompleteLayout>
-          </PrivateRoute>
-        }
-      ></Route>
-      <Route
-        path="/auth/esqueci-senha"
-        element={<ForgotPasswordForm />}
-      ></Route>
-      <Route
-        path="/login"
-        element={<LoginForm />}
-      ></Route>
-      <Route
-        path="/registrar"
-        element={<RegisterForm />}
-      ></Route>
-      <Route
-        path="/auth/trocar-senha"
-        element={<ResetPasswordForm />}
-      ></Route>
-      <Route
-        path="/auth/verificar-codigo"
-        element={<VerifyForm />}
-      ></Route>
     </Routes>
   );
 }
